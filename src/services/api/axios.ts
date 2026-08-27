cc1e8bd9acc34bdd6e49e557e31337ef2e160dbb
EktorVT <hector2001rdz@gmail.com>
2026-08-27 13:30:35 -0600

    Feat [3] Add axios interceptor

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -7,4 +7,20 @@ const api = axios.create({
   }
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("access_token");
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
